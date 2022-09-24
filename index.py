0d0d430c1df741226d92fb8cc6ca54e87755a046
Aman Singh <72974096+amasin-26@users.noreply.github.com>
2022-09-24 21:33:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ def LongestCompoundWord(original_trie, trie, word, level=0):
   return LongestCompoundWord(original_trie, trie[first_letter], word[1:], level)
 
 #Words that were in your question
-f=open("Input_02.txt","r")
+f=open("Input_01.txt","r")
 s=f.read()
 words =list(map(str,s.split("\n")))
 trie = Trie(words)
